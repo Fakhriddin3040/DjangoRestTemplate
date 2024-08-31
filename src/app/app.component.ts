1f9f7ddcaec6cf1c894b7d19b22697a46797d07f
olimov Amir <olimov.amir@mail.ru>
2024-08-31 08:06:20 +0300

    has been created component header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.component.html',  
+  styleUrls: ['./app.component.scss'],   
 })
 export class AppComponent {
   title = 'marketplace-client';
