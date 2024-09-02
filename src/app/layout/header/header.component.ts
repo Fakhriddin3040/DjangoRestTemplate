f12aa5eb3c11ca695370b35ee10e8c76dbfe8ffc
olimov Amir <olimov.amir@mail.ru>
2024-09-02 14:23:58 +0300

    The material library is installed

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule
+  ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   
