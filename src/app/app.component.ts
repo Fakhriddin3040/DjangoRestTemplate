5f5da95e5ae4b546a1bf7332f1ba1dffec286720
olimov Amir <olimov.amir@mail.ru>
2024-09-06 14:28:28 +0300

    has been created component main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
+import { MainComponent } from "./layout/main/main.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MainComponent],
   templateUrl: './app.component.html',  
   styleUrls: ['./app.component.scss'],   
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    
+];
