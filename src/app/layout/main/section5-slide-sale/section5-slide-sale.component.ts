a0c335a2f5e28c725fa5b9c52c72b0a4df0f5ab0
olimov Amir <olimov.amir@mail.ru>
2024-09-11 12:35:19 +0300

    fix bug slide

--- src/app/layout/main/section5-slide-sale/section5-slide-sale.component.ts
+++ src/app/layout/main/section5-slide-sale/section5-slide-sale.component.ts
@@ -1,13 +1,62 @@
-import { Component } from '@angular/core';
-import { ContainerComponent } from "../../../shared/container/container.component";
+import { Component, OnInit, AfterViewInit, Renderer2, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { ContainerComponent } from '../../../shared/container/container.component';
 
 @Component({
   selector: 'app-section5-slide-sale',
+  templateUrl: './section5-slide-sale.component.html',
   standalone: true,
   imports: [ContainerComponent],
-  templateUrl: './section5-slide-sale.component.html',
-  styleUrl: './section5-slide-sale.component.scss'
+  styleUrls: ['./section5-slide-sale.component.scss']
 })
-export class Section5SlideSaleComponent {
+export class Section5SlideSaleComponent implements OnInit, AfterViewInit {
+  private scriptLoaded = false;
 
+  constructor(
+    private renderer: Renderer2,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      // Загружаем скрипт и стили для Swiper, если они еще не загружены
+      const script = this.renderer.createElement('script');
+      script.src = 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js';
+      script.onload = () => {
+        this.scriptLoaded = true;
+        this.initializeSwiper();
+      };
+      script.onerror = (error: any) => {
+        console.error('Ошибка загрузки скрипта Swiper:', error);
+      };
+      this.renderer.appendChild(document.body, script);
+
+      const link = this.renderer.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.css';
+      this.renderer.appendChild(document.head, link);
+    }
+  }
+
+  ngAfterViewInit(): void {
+    if (this.scriptLoaded) {
+      this.initializeSwiper();
+    }
+  }
+
+  private initializeSwiper(): void {
+    if (window['Swiper']) {
+      const Swiper = window['Swiper'];
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    } else {
+      console.error('Swiper не найден.');
+    }
+  }
 }
---
+++ src/types/bootstrap.d.ts
@@ -0,0 +1,6 @@
+declare module 'bootstrap' {
+    export class Carousel {
+      constructor(element: Element, options?: any);
+      to(index: number): void;
+    }
+  }
