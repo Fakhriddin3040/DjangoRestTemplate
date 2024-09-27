6ed834ceb74de2c4624f45c1cf4a50ca09a42e80
olimov Amir <olimov.amir@mail.ru>
2024-09-27 12:09:43 +0300

    has been created component app-menu

---
+++ src/app/shared/components/menu/app-menu/app-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-menu.component.html',
+  styleUrl: './app-menu.component.scss'
+})
+export class AppMenuComponent {
+
+}
