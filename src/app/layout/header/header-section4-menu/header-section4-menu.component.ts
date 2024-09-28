cf6431f7a61163497a97df27d28238ce5c080e22
olimov Amir <olimov.amir@mail.ru>
2024-09-28 13:30:52 +0300

    fix bug

--- src/app/layout/header/header-section4-menu/header-section4-menu.component.ts
+++ src/app/layout/header/header-section4-menu/header-section4-menu.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../../shared/container/container.component';
-import { AppMenuComponent } from "../../../shared/components/menu/app-menu/app-menu.component";
-import { MenuHomeLayoutComponent } from "../../../shared/components/menu/menu-home-layout/menu-home-layout.component";
-import { MenuCategoryComponent } from "../../../shared/components/menu/menu-category/menu-category.component";
-import { MenuProductComponent } from "../../../shared/components/menu/menu-product/menu-product.component";
-import { MenuBlogComponent } from "../../../shared/components/menu/menu-blog/menu-blog.component";
+import { AppMenuComponent } from '../../../shared/components/menu/app-menu/app-menu.component';
+import { MenuHomeLayoutComponent } from '../../../shared/components/menu/menu-home-layout/menu-home-layout.component';
+import { MenuCategoryComponent } from '../../../shared/components/menu/menu-category/menu-category.component';
+import { MenuProductComponent } from '../../../shared/components/menu/menu-product/menu-product.component';
+import { MenuBlogComponent } from '../../../shared/components/menu/menu-blog/menu-blog.component';
+
+
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header-section4-menu',
   standalone: true,
-  imports: [ContainerComponent, AppMenuComponent, MenuHomeLayoutComponent, MenuCategoryComponent, MenuProductComponent, MenuBlogComponent],
+  imports: [
+    ContainerComponent,
+    AppMenuComponent,
+    MenuHomeLayoutComponent,
+    MenuCategoryComponent,
+    MenuProductComponent,
+    MenuBlogComponent,
+    MatButtonModule
+  ],
   templateUrl: './header-section4-menu.component.html',
-  styleUrl: './header-section4-menu.component.scss'
+  styleUrl: './header-section4-menu.component.scss',
 })
-export class HeaderSection4MenuComponent {
-
-}
+export class HeaderSection4MenuComponent {}
