56f050b92433fa100372a89c6e7a5e4a8cedb9f3
olimov Amir <olimov.amir@mail.ru>
2024-10-02 06:44:42 +0300

    has been created component product

---
+++ src/app/features/products/components/product-list/product-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.scss'
+})
+export class ProductListComponent {
+
+}
