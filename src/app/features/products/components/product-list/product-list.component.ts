66554845c877617946c8ec9ee15118a4ac55d5d0
olimov Amir <olimov.amir@mail.ru>
2024-10-02 08:16:54 +0300

    fix bug style

--- src/app/features/products/components/product-list/product-list.component.ts
+++ src/app/features/products/components/product-list/product-list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BreadcrumbNavigationComponent } from "../../../../shared/components/breadcrumb-navigation/breadcrumb-navigation.component";
+import { ContainerComponent } from "../../../../shared/container/container.component";
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [BreadcrumbNavigationComponent, ContainerComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
