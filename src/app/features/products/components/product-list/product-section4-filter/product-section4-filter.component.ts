0bdb6d42be34febaa5eafbf7b6c5a5dde072d92a
olimov Amir <olimov.amir@mail.ru>
2024-10-05 14:26:56 +0300

    fix

--- src/app/features/products/components/product-list/product-section4-filter/product-section4-filter.component.ts
+++ src/app/features/products/components/product-list/product-section4-filter/product-section4-filter.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductSettingFilterComponent } from "./product-setting-filter/product-setting-filter.component";
+import { ProductShowComponent } from "./product-show/product-show.component";
 
 @Component({
   selector: 'app-product-section4-filter',
   standalone: true,
-  imports: [],
+  imports: [ProductSettingFilterComponent, ProductShowComponent],
   templateUrl: './product-section4-filter.component.html',
   styleUrl: './product-section4-filter.component.scss'
 })
