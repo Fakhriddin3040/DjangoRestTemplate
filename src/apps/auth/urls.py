c42ccea1045ecdcc3d595230e5ea7b252dc760f1
Fakhriddin <fahriddin3040@gmail.com>
2024-10-12 14:48:09 +0500

    Update urls.py

--- src/apps/auth/urls.py
+++ src/apps/auth/urls.py
@@ -8,19 +8,18 @@ router = DefaultRouter()
 urlpatterns = [
     path("", include(router.urls)),
     path("login/", user_views.UserLoginAPIView.as_view()),
-    # path("register/", user_views.UserRegisterAPIView.as_view()),
-    path("phone_register/step1", user_views.UserPhoneStep1RegisterAPIView.as_view()),
-    path("phone_register/step2", user_views.UserPhoneStep2RegisterAPIView.as_view()),
-    path("phone_register/step3", user_views.UserPhoneStep3RegisterAPIView.as_view()),
+    path("registration/phone/1/", user_views.UserPhoneStep2RegisterAPIView.as_view()),
+    path("registration/phone/2/", user_views.UserPhoneStep1RegisterAPIView.as_view()),
+    path("registration/phone/3/, user_views.UserPhoneStep3RegisterAPIView.as_view()),
     path(
-        "email_registration/step1", user_views.UserEmailStep1RegisterAPIView.as_view()
+        "registration/email/1/", user_views.UserEmailStep1RegisterAPIView.as_view()
     ),
     path(
-        "email_registration/step2", user_views.UserEmailStep2RegisterAPIView.as_view()
+        "registration/email/2/",  user_views.UserEmailStep2RegisterAPIView.as_view()
     ),
     path(
-        "email_registration/step3", user_views.UserEmailStep3RegisterAPIView.as_view()
+        "registration/email/3/", user_views.UserEmailStep3RegisterAPIView.as_view()
     ),
-    path("oauth-register/", user_views.UserOAuthRegisterAPIView.as_view()),
-    path("oauth-login/", user_views.UserOAuthLoginAPIView.as_view()),
+    path("registration/oauth2/", user_views.UserOAuthRegisterAPIView.as_view()),
+    path("login/oauth2/", user_views.UserOAuthLoginAPIView.as_view()),
 ]
