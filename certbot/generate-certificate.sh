6e86a32b6f5072a3e712efc48403d86d31603206
Github Acgtions <market_gha@rablakznrz.local>
2024-11-15 15:17:05 +0000

    Reconfigurating certbot.
    Signed-off-by: Github Acgtions <market_gha@rablakznrz.local>

--- certbot/generate-certificate.sh
+++ certbot/generate-certificate.sh
@@ -3,16 +3,16 @@
 # Убедитесь, что предыдущие сертификаты удалены
 rm -rf /etc/letsencrypt/live/certfolder*
 
-certbot certonly --standalone \
-    --webroot -w /var/www/certbot \
+certbot certonly --webroot -w /var/www/certbot \
     --non-interactive \
     --email $DOMAIN_EMAIL \
     -d $DOMAIN_URL \
     --agree-tos
 
 # Проверка успешности получения сертификатов
-if [ -f /etc/letsencrypt/live/certfolder/fullchain.pem ] && [ -f /etc/letsencrypt/live/certfolder/privkey.pem ]; then
+if [ -f /etc/letsencrypt/live/astrotech-spaceapp.space/fullchain.pem ] then
     echo "Сертификаты были успешно сгенерированы."
 else
     echo "Сертификаты не были сгенерированы. Проверьте настройки."
 fi
+
