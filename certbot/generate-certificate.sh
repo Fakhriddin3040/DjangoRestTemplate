7394b5e3f7849dc75dd9336c0abffac58ceb452f
Fakhriddin <fahriddin3040@gmail.com>
2024-11-15 18:24:47 +0500

    adding ssl certificate.
    Signed-off-by: Fakhriddin <fahriddin3040@gmail.com>

--- certbot/generate-certificate.sh
+++ certbot/generate-certificate.sh
@@ -3,21 +3,16 @@
 # Убедитесь, что предыдущие сертификаты удалены
 rm -rf /etc/letsencrypt/live/certfolder*
 
-# Запустите certbot с опцией --non-interactive
 certbot certonly --standalone \
-    --webroot-path /var/www/certbot \
+    --webroot -w /var/www/certbot \
     --non-interactive \
     --email $DOMAIN_EMAIL \
     -d $DOMAIN_URL \
-    --cert-name=certfolder \
-    --key-type rsa \
     --agree-tos
 
 # Проверка успешности получения сертификатов
 if [ -f /etc/letsencrypt/live/certfolder/fullchain.pem ] && [ -f /etc/letsencrypt/live/certfolder/privkey.pem ]; then
-    # Копируем сертификаты в папку Nginx
-    cp /etc/letsencrypt/live/certfolder/fullchain.pem /etc/nginx/cert.pem
-    cp /etc/letsencrypt/live/certfolder/privkey.pem /etc/nginx/key.pem
+    echo "Сертификаты были успешно сгенерированы."
 else
     echo "Сертификаты не были сгенерированы. Проверьте настройки."
 fi
