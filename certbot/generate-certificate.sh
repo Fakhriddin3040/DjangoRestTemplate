151555a4d837f4e8308895fc58f995d0b26fe46c
Github Acgtions <market_gha@rablakznrz.local>
2024-11-16 04:09:50 +0000

    Some fixes and refactoring
    Signed-off-by: Fakhriddin <fahriddin3040@gmail.com>

--- certbot/generate-certificate.sh
+++ certbot/generate-certificate.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 NGINX_CONF_DIR="/etc/nginx/conf.d"
-SSL_CONF="/ssl.conf"
 
 # Проверяем наличие ssl.conf
 if [ -f "$NGINX_CONF_DIR/ssl.conf" ]; then
-    echo "Перемещаю $NGINX_CONF_DIR/ssl.conf в корень..."
-    mv "$NGINX_CONF_DIR/ssl.conf" "$SSL_CONF"
+    echo "Перемещаем $NGINX_CONF_DIR/ssl.conf..."
+    mv "$NGINX_CONF_DIR/ssl.conf" /
 fi
 
 # Запуск certbot для генерации сертификатов
@@ -18,10 +17,37 @@ certbot certonly --webroot -w /var/www/certbot \
     --agree-tos
 
 
-# Возвращаем ssl.conf, если он был перемещён
+# Возвращаем ssl.conf, если он был удалён
 if [ ! -f "$NGINX_CONF_DIR/ssl.conf" ]; then
-    echo "Возвращаю $SSL_CONF в $NGINX_CONF_DIR..."
-    mv "$SSL_CONF" "$NGINX_CONF_DIR/ssl.conf"
+    echo "Возвращаем ssl.conf в $NGINX_CONF_DIR..."
+    mv "/ssl.conf" "$NGINX_CONF_DIR/ssl.conf"
+else
+    echo "$NGINX_CONF_DIR/ssl.conf уже существует"
+fi
+
+echo "Скрипт завершён успешно."#!/bin/sh
+
+NGINX_CONF_DIR="/etc/nginx/conf.d"
+
+# Проверяем наличие ssl.conf
+if [ -f "$NGINX_CONF_DIR/ssl.conf" ]; then
+    echo "Перемещаем $NGINX_CONF_DIR/ssl.conf..."
+    mv "$NGINX_CONF_DIR/ssl.conf" /
+fi
+
+# Запуск certbot для генерации сертификатов
+echo "Запускаю certbot..."
+certbot certonly --webroot -w /var/www/certbot \
+    --non-interactive \
+    --email "$DOMAIN_EMAIL" \
+    -d "$DOMAIN_URL" \
+    --agree-tos
+
+
+# Возвращаем ssl.conf, если он был удалён
+if [ ! -f "$NGINX_CONF_DIR/ssl.conf" ]; then
+    echo "Возвращаем ssl.conf в $NGINX_CONF_DIR..."
+    mv "/ssl.conf" "$NGINX_CONF_DIR/ssl.conf"
 else
     echo "$NGINX_CONF_DIR/ssl.conf уже существует"
 fi
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,11 +3,26 @@
 # Установить необходимые пакеты
 apk add --no-cache bash dcron
 
+# Проверить доступ к Docker
+docker ps > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Docker socket недоступен!"
+    exit 1
+fi
+
 # Запустить cron
 crond
 
 # Запустить планировщик
-/bin/bash /scripts/certbot_schedule.sh
+sh /scripts/certbot_schedule.sh
+
+# Подождать 10 секунд
+sleep 10
+
+# Перезапустить NGINX
+echo "Перезапускаем прокси сервер"
+
+docker container ps --filter "name=proxy" --format="{{.Names}}"
 
 # Удерживать контейнер активным
 tail -f /dev/null
