a3b1a720339c6d6ebc5cbb7b894089fb48831399
Github Acgtions <market_gha@rablakznrz.local>
2024-11-16 11:53:07 +0000

    ...
    Signed-off-by: Github Acgtions <market_gha@rablakznrz.local>

--- certbot/generate-certificate.sh
+++ certbot/generate-certificate.sh
@@ -9,34 +9,7 @@ if [ -f "$NGINX_CONF_DIR/ssl.conf" ]; then
 fi
 
 # Запуск certbot для генерации сертификатов
-echo "Запускаю certbot..."
-certbot certonly --webroot -w /var/www/certbot \
-    --non-interactive \
-    --email "$DOMAIN_EMAIL" \
-    -d "$DOMAIN_URL" \
-    --agree-tos
 
-
-# Возвращаем ssl.conf, если он был удалён
-if [ ! -f "$NGINX_CONF_DIR/ssl.conf" ]; then
-    echo "Возвращаем ssl.conf в $NGINX_CONF_DIR..."
-    mv "/ssl.conf" "$NGINX_CONF_DIR/ssl.conf"
-else
-    echo "$NGINX_CONF_DIR/ssl.conf уже существует"
-fi
-
-echo "Скрипт завершён успешно."#!/bin/sh
-
-NGINX_CONF_DIR="/etc/nginx/conf.d"
-
-# Проверяем наличие ssl.conf
-if [ -f "$NGINX_CONF_DIR/ssl.conf" ]; then
-    echo "Перемещаем $NGINX_CONF_DIR/ssl.conf..."
-    mv "$NGINX_CONF_DIR/ssl.conf" /
-fi
-
-# Запуск certbot для генерации сертификатов
-echo "Запускаю certbot..."
 certbot certonly --webroot -w /var/www/certbot \
     --non-interactive \
     --email "$DOMAIN_EMAIL" \
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,26 +3,41 @@
 # Установить необходимые пакеты
 apk add --no-cache bash dcron
 
-# Проверить доступ к Docker
-docker ps > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-    echo "Docker socket недоступен!"
+if ! docker ps > /dev/null 2>&1; then
+    echo "Error: Docker socket not available."
     exit 1
+else
+    echo "Docker is available. Continue processes."
 fi
 
 # Запустить cron
+echo "Running cron..."
 crond
 
 # Запустить планировщик
-sh /scripts/certbot_schedule.sh
+if [ -f /scripts/certbot_schedule.sh ]; then
+    echo "Running Certbot scheduler..."
+    sh /scripts/certbot_schedule.sh
+else
+    echo "Error: Scheduler script \
+        /scripts/certbot_schedule.sh not found."
+fi
 
 # Подождать 10 секунд
+echo "Waiting 10 seconds..."
 sleep 10
 
 # Перезапустить NGINX
-echo "Перезапускаем прокси сервер"
+echo "Restarting proxy server(NGINX)..."
+proxy_container=$(docker container ps --filter "name=proxy" --format="{{.Names}}")
 
-docker container ps --filter "name=proxy" --format="{{.Names}}"
+if [ -n "$proxy_container" ]; then
+    docker restart "$proxy_container"
+    echo "Proxy server ($proxy_container) successfuly restarted."
+else
+    echo "Error: Container service 'proxy' not found."
+fi
 
 # Удерживать контейнер активным
+echo "Holding container active."
 tail -f /dev/null
