51ccba95d1d9b0989cfb6fc6f3478064aafdbdda
Fakhriddin <fahriddin3040@gmail.com>
2024-11-22 11:11:03 +0500

    Admin for product
    Signed-off-by: Fakhriddin <fahriddin3040@gmail.com>

--- src/apps/product/admin/product.py
+++ src/apps/product/admin/product.py
@@ -2,8 +2,13 @@ from django.contrib.admin import ModelAdmin
 
 
 class ProductAdmin(ModelAdmin):
-    list_display = ("id", "title", "price", "category__title", "count")
+    list_display = ("id", "title", "price", "display_category", "count")
     list_display_links = ("id", "title")
     readonly_fields = ("ext_id", "ext_cat_id", "category", "count")
     search_fields = ("title",)
     list_filter = ("category",)
+
+    def display_category(self, obj):
+        return obj.category.title if obj.category else "-"
+
+    display_category.short_description = "Категория"
